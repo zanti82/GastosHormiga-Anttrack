36786d2f28eeb6913b57fd32f772605a85048b81
zanti82 <zanti82@gmail.com>
2026-06-06 20:07:25 -0500

    urlngrok

--- app-ant-track-FrontendII/src/services/api.js
+++ app-ant-track-FrontendII/src/services/api.js
@@ -2,8 +2,7 @@
 
 //const URL_BASE = "http://3.134.93.54:8080/anttrackapi/v1";
 const URL_BASE = "https://abc123.ngrok-free.app/anttrackapi/v1";
-
-//actaluzando
+//nueva linea
 
 export let endPoints = {
 
